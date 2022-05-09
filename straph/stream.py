5676020a9bacc969af679f7344f4c484de8a1102
Mehdi Naima <olmehdiol@gmail.com>
2022-05-09 12:53:46 +0200

    dijkstra with T factor

--- straph/stream.py
+++ straph/stream.py
@@ -5227,75 +5227,7 @@ class StreamGraph:
                     #     else:
                     #         pre[b][second_arrival][(a,e)] = edge_taken2
         return
-
-    def relax_resting_aux_dij(self, b, last_depar, arrival, e, cur_best, pre, Q, Q_nod, P):
-        if (b,arrival) in P:
-            return
-        if arrival in cur_best[b]:
-            cnew = self.compute_c(last_depar, arrival, cur_best[b][e][1])
-            cold = self.compute_c(cur_best[b][arrival][0], arrival, cur_best[b][arrival][1])
-            #if c < ((arrival - cur_best[b][arrival][0]), cur_best[b][arrival][1]):
-            if cnew < cold:
-                #pre[b][arrival] = set()
-                pre[b][arrival] = dict()
-                cur_best[b][arrival] = (last_depar,cur_best[b][e][1])
-                #Q.decrease_key(Q_nod[b,arrival], (cnew,(b,arrival)) )
-                print(arrival, last_depar, cnew,cold)
-                Q.decrease_key(Q_nod[b,arrival], (cnew,(b,arrival)))
-
-    def relax_resting_paths_dij(self, b, t1, t2, pre, cur_best, events, events_rev, Q, Q_nod, P):
-        #arrivals is a dictionary of integers that represent last_arrival, the value of each element in the dict is a couple (last_departure, length of metawalk)
-        arrivals_b = [e for e in events if (e in pre[b]) and (pre[b][e] != {}) ]
-        close_arrival = self.closest_arrival(t2, arrivals_b)
-        #print("close_arrival",close_arrival, t2)
-        if close_arrival != -1:
-            last_depar = cur_best[b][close_arrival][0]
-            for i in range(events_rev[close_arrival],len(events)):
-            #for i in [events_rev[t1],events_rev[t2]]:
-                self.relax_resting_aux_dij(b, last_depar, events[i], close_arrival, cur_best, pre, Q, Q_nod, P)
-
-        return
-
-    def relax_paths_aux_dij(self, a, b, last_depar, arrival, e, edge_taken, cur_best, pre, Q, Q_nod, P):
-        if (b,arrival) in P:
-            return
-        cnew = self.compute_c(last_depar, arrival, cur_best[a][e][1] + 1)
-        cold = self.compute_c(cur_best[b][arrival][0], arrival, cur_best[b][arrival][1])
-        #if c < ((arrival - cur_best[b][arrival][0]), cur_best[b][arrival][1]):
-        if cnew < cold:
-            #pre[b][arrival] = set()
-            pre[b][arrival] = dict()
-            cur_best[b][arrival] = (last_depar,cur_best[a][e][1] + 1)
-            cold = self.compute_c(cur_best[b][arrival][0], arrival, cur_best[b][arrival][1])
-            #print("Q_nod[b,arrival]",Q_nod[b,arrival])
-            Q.decrease_key(Q_nod[b,arrival], (cnew,(b,arrival)))
-            #Q.decrease_key(Q_nod[b,arrival], (cnew, (b,arrival)) )
-        if cnew == cold:
-            #pre[b][arrival].add((a,e,edge_taken))
-            if (a,e) not in pre[b][arrival]:
-                pre[b][arrival][(a,e)] = edge_taken
-            else:
-                e1,e2 = edge_taken
-                x1,x2 = pre[b][arrival][(a,e)]
-                if (e1 == x1 and e2 > x2) or (e2 == x2 and e1 < x1):
-                    pre[b][arrival][(a,e)] = edge_taken
-
-    def relax_paper_dij(self, a, b, t, t1, t2, pre, cur_best, events, Q, Q_nod, P):
-        #arrivals = [e for e in events if (e in pre[a]) and (pre[a][e] != {})]
-        arrivals = [t1,t2]
-        for i in range(len(arrivals)):
-            e = arrivals[i]
-            #print("arrivals",arrivals)
-            last_depar = cur_best[a][e][0]
-            first_arrival, second_arrival, edge_taken1, edge_taken2 = self.arrival(e, t1, t2)
-            if (first_arrival, second_arrival, edge_taken1, edge_taken2) != (-1,-1,-1,-1):
-                self.relax_paths_aux_dij(a, b, last_depar, first_arrival, e, edge_taken1, cur_best, pre, Q, Q_nod, P)
-
-                if second_arrival != -1:
-                    self.relax_paths_aux_dij(a, b, last_depar, second_arrival, e, edge_taken2, cur_best, pre, Q, Q_nod, P)
-        return
-
-    # def add_curbest_initialise_pre(self, b, t, cur_best, pre):
+        # def add_curbest_initialise_pre(self, b, t, cur_best, pre):
     #     if t not in cur_best[b]:
     #         cur_best[b][t] = (t,1)
     #     else:
@@ -5343,6 +5275,80 @@ class StreamGraph:
 
         return (pre, cur_best)
 
+
+    def relax_resting_aux_dij(self, b, last_depar, arrival, e, cur_best, pre, Q, Q_nod, P):
+        if arrival in P[b]:
+            return
+        if arrival in cur_best[b]:
+            cnew = self.compute_c(last_depar, arrival, cur_best[b][e][1])
+            cold = self.compute_c(cur_best[b][arrival][0], arrival, cur_best[b][arrival][1])
+            #if c < ((arrival - cur_best[b][arrival][0]), cur_best[b][arrival][1]):
+            if cnew < cold:
+                #pre[b][arrival] = set()
+                pre[b][arrival] = dict()
+                cur_best[b][arrival] = (last_depar,cur_best[b][e][1])
+                #Q.decrease_key(Q_nod[b,arrival], (cnew,(b,arrival)) )
+                print(arrival, last_depar, cnew,cold)
+                if (b,arrival) in Q_nod:
+                    Q.decrease_key(Q_nod[b,arrival], (cnew,(b,arrival)))
+
+    def relax_resting_paths_dij(self, b, t1, t2, pre, cur_best, events, events_rev, Q, Q_nod, P):
+        #arrivals is a dictionary of integers that represent last_arrival, the value of each element in the dict is a couple (last_departure, length of metawalk)
+        arrivals_b = [e for e in events if (e in pre[b]) and (pre[b][e] != {}) ]
+        close_arrival = self.closest_arrival(t2, arrivals_b)
+        #print("close_arrival",close_arrival, t2)
+        if close_arrival != -1:
+            last_depar = cur_best[b][close_arrival][0]
+            for i in range(events_rev[close_arrival],len(events)):
+                self.relax_resting_aux_dij(b, last_depar, events[i], close_arrival, cur_best, pre, Q, Q_nod, P)
+
+        return
+
+    def relax_paths_aux_dij(self, a, b, last_depar, arrival, e, edge_taken, cur_best, pre, Q, Q_nod, P):
+        print("a",a,"e",e,"b",b,"arrival",arrival)
+        if arrival in P[b]:
+            return
+        cnew = self.compute_c(last_depar, arrival, cur_best[a][e][1] + 1)
+        cold = self.compute_c(cur_best[b][arrival][0], arrival, cur_best[b][arrival][1])
+        print("cnew",cnew,"cold",cold)
+        #if c < ((arrival - cur_best[b][arrival][0]), cur_best[b][arrival][1]):
+        if cnew < cold:
+            #pre[b][arrival] = set()
+            pre[b][arrival] = dict()
+            cur_best[b][arrival] = (last_depar,cur_best[a][e][1] + 1)
+            cold = self.compute_c(cur_best[b][arrival][0], arrival, cur_best[b][arrival][1])
+            #print("Q_nod[b,arrival]",Q_nod[b,arrival])
+            if (b,arrival) in Q_nod:
+                Q.decrease_key(Q_nod[b,arrival], (cnew,(b,arrival)))
+            #Q.decrease_key(Q_nod[b,arrival], (cnew, (b,arrival)) )
+        if cnew == cold:
+            #pre[b][arrival].add((a,e,edge_taken))
+            if (a,e) not in pre[b][arrival]:
+                pre[b][arrival][(a,e)] = edge_taken
+            else:
+                e1,e2 = edge_taken
+                x1,x2 = pre[b][arrival][(a,e)]
+                if (e1 == x1 and e2 > x2) or (e2 == x2 and e1 < x1):
+                    pre[b][arrival][(a,e)] = edge_taken
+
+    def relax_paper_dij(self, a, b, t, t1, t2, pre, cur_best, events, Q, Q_nod, P):
+        #arrivals = [e for e in events if (e in pre[a]) and (pre[a][e] != {})]
+        arrivals = []
+        if pre[a][t] != {}:
+            arrivals = [t ]
+        for i in range(len(arrivals)):
+            e = arrivals[i]
+            #print("arrivals",arrivals)
+            last_depar = cur_best[a][e][0]
+            first_arrival, second_arrival, edge_taken1, edge_taken2 = self.arrival(e, t1, t2)
+            if (first_arrival, second_arrival, edge_taken1, edge_taken2) != (-1,-1,-1,-1):
+                self.relax_paths_aux_dij(a, b, last_depar, first_arrival, e, edge_taken1, cur_best, pre, Q, Q_nod, P)
+
+                if second_arrival != -1:
+                    self.relax_paths_aux_dij(a, b, last_depar, second_arrival, e, edge_taken2, cur_best, pre, Q, Q_nod, P)
+        return
+
+
     def neighbors(self):
         res =[[] for i in range(len(self.nodes))]
         for e in self.links:
@@ -5361,7 +5367,9 @@ class StreamGraph:
         pre = dict()
         cur_best = dict()
         nod = dict()
-        P = set()
+        P = dict()
+        for v in self.nodes:
+            P[v] = set()
         for v in self.nodes:
             cur_best[v] = dict()
             pre[v] = dict()
@@ -5370,8 +5378,19 @@ class StreamGraph:
                 pre[v][t] = {}
                 if v == s:
                     cur_best[v][t] = (t,0)
-                #fheappush(Q, ((t - cur_best[v][t][0],cur_best[v][t][1]),(v,t) ))
+                    pre[v][t]={(0,0):(-1,-1)}
                 nod[v,t] = Q.insert( ((t - cur_best[v][t][0],cur_best[v][t][1]),(v,t) ) )
+                #fheappush(Q, ((t - cur_best[v][t][0],cur_best[v][t][1]),(v,t) ))
+        # for i in range(0,len(self.links)):
+        #     a,b = self.links[i]
+        #     for j in range(0,len(self.link_presence[i]),2):
+        #         for z in range(0,2):
+        #             v,t = a,self.link_presence[i][j+z]
+        #             nod[v,t] = Q.insert( ((t - cur_best[v][t][0],cur_best[v][t][1]),(v,t) ) )
+        #         for z in range(0,2):
+        #             v,t = b,self.link_presence[i][j+z]
+        #             nod[v,t] = Q.insert( ((t - cur_best[v][t][0],cur_best[v][t][1]),(v,t) ) )
+
         print(Q.total_nodes)
         while Q.total_nodes != 0:
             #mm = Q.find_min()
@@ -5380,11 +5399,12 @@ class StreamGraph:
             #(x,y) = q.key
             print("nb_nodes", Q.total_nodes,"min",Q.find_min().data)
             (x,y) = Q.extract_min().data
-            print(cur_best)
+            for jjj in self.nodes:
+                print(jjj,pre[jjj])
             print(Q.total_nodes, "extracted", y,x)
 
             (a,t) = y
-            P.add((a,t))
+            P[a].add(t)
             (tp,dis) = x
             for b in neighbors[a]:
                 (xx,yy) = (a,b)
