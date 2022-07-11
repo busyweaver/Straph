ddd907b9245b96fe28e3c7e7c211e882c420db61
Mehdi Naima <olmehdiol@gmail.com>
2022-07-11 16:03:38 +0200

    remove printing

--- straph/betweenness/general_contributions_from_node.py
+++ straph/betweenness/general_contributions_from_node.py
@@ -1,70 +1,44 @@
 from straph.betweenness import volumes as vol
 import math
 def contri_delta_svvt_con(s, v, t, lat, contri, prev_next, sigma_r, lat_rev):
-    #if (v,t) in deltasvvt:
-        #return deltasvvt[(v,t)]
-    print("///////// call svvt, ","s",s,"v",v,"t",t)
     if s == v or t not in lat[v]:
         return vol.Volume(0,0)
     #voir papier matthieu clemence pour l'algo
     prev = []
     next = []
-
-
     if t in prev_next[v]:
         prev = [lat[v][e][0] for e in prev_next[v][t] if e < t]
         next = [e for e in prev_next[v][t] if e > t]
-
     prev =  prev + [contri[v][t][0]]
     next = next + [contri[v][t][1]]
     left = vol.Volume(0,0)
     right = vol.Volume(0,0)
-
     contrib = vol.Volume(0,0)
     s_prime = vol.Volume(lat[v][t][0], 0)
     vol_tv = sigma_r[(v,t)]
-
     for s_left in prev:
-
         a_prime = vol.Volume(t,0)
         right = vol.Volume(0,0)
         for a_right in next:
-
-            print("s_prime", s_prime, "s_left", s_left, "a_right", a_right, "a_prime", a_prime)
             enum = (s_prime - vol.Volume(s_left,0)) * (vol.Volume(a_right,0) - a_prime) * vol_tv
-
-            print("left", left, "vol_tv", vol_tv, "right", right)
             denum = left + vol_tv + right
-            print("enum", enum, "denum", denum)
-            #print("enum-denum degree",enum_degree, denum_degree)
             if denum !=vol.Volume(0,0):
                 res = enum / denum
             else:
                 res = vol.Volume(0,0)
-            #contrib += (enum/denum)
             contrib += res
-            print("contrib",contrib)
-            #if pointer[(v,a_right)] in sigma_r:
             if a_right in lat[v]:
-                print("**************", right,sigma_r[(v,a_right)],"********************" )
                 right += sigma_r[(v,a_right)]
-            # else:
-            #     right = 0
             a_prime = vol.Volume(a_right, 0)
-        #if pointer[(v,lat_rev[v][s_left])] in sigma_r:
+
         if (s_left in lat_rev[v]) and (lat_rev[v][s_left][0] in lat[v]):
-        #if lat_rev[v][s_left] in lat[v]:
+
             left += sigma_r[(v,lat_rev[v][s_left][0])]
-        # else:
-        #     left = 0
         s_prime = vol.Volume(s_left, 0)
-    print("end svvt", contrib)
-    #deltasvvt[(v,t)] = contrib
     return contrib
 
 
 def kappa(r,tp,tp_prev):
-    print("tp",tp,"tp_prev",tp_prev)
     return vol.Volume((tp-tp_prev)/math.factorial(r+1) , r+1)
 
 def prev_event(tp,event,event_rev):
@@ -74,50 +48,30 @@ def prev_event(tp,event,event_rev):
 
 def contri_intermeidary_vertices(v, t, w, t_p, l_nei, partial_sum, contrib_local, ii, jj, sigma_r, event, event_reverse, contribution):
     for jjj in range(jj+1,event_reverse[l_nei[v,t][ii][0]]+1):
-        print("v", v, "t",t,"event[jj]", event[jj], "dic_nodes[u][ii]", "index actual event",jj,"index succ events", jjj, "contri event time" ,event[jjj])
-        print("comp vol", sigma_r[v,t], sigma_r[v,event[jjj]])
-
         if v not in contrib_local:
             contrib_local[v] = dict()
         if not (v in contribution and event[jjj] in contribution[v]):
-            if sigma_r[v,t] != sigma_r[v,event[jjj]]:
-                print("ERREUUUUUUUR ",sigma_r[v,t],sigma_r[v,event[jjj]])
-            print("add_contri_local","v",v,"event[jjj]",event[jjj],"w,t_p",w,t_p)
             if event[jjj] != t_p:
-                print("ici")
                 contrib_local[v][event[jjj]] = partial_sum[l_nei[v,t][ii][0]]
             else:
-                print("la","contribution[w][t_p]",contribution[w][t_p], "ii", ii, "len(l_nei[v,t])-1",len(l_nei[v,t])-1)
                 if ii == len(l_nei[v,t])-1:
-                    #contrib_local[v][event[jjj]] =  contribution[w][t_p]*(self.coef_volume_con(node, v,event[jjj],w,t_p,sigma_r, pre, nppol.Polynomial([1]) ))
                     contrib_local[v][event[jjj]]= (sigma_r[(v,event[jjj])]/sigma_r[(w,t_p)] ) *contribution[w][t_p]
                 else:
-                    print("partial_sum",partial_sum[l_nei[v,t][ii+1][0]], "l_nei[v,t]",l_nei[v,t])
                     contrib_local[v][event[jjj]] = partial_sum[l_nei[v,t][ii+1][0]]  + (sigma_r[(v,event[jjj])]/sigma_r[(w,t_p)] )*contribution[w][t_p]
-                    #(self.coef_volume_con(node, v,event[jjj],w,t_p,sigma_r, pre, nppol.Polynomial([1])))
-
-
-
 
 def contri_delta_svt(node, v, t, l_nei, sigma_r, contribution, deltasvvt, event, event_reverse, pre, GT, unt):
-    print("******** new call contri_delta_svt","v", v, "t", t)
     if (v not in contribution) or ((v in contribution) and (t not in contribution[v])):
         partial_sum = dict()
         s = vol.Volume(0,0)
         contrib_local = dict()
-        # l_ord = list(dic_nodes_rev.keys())
-        # l_ord.sort()
         for ii in range(len(l_nei[(v,t)])-1,-1,-1):
             #normally it should be sorted
             for u in l_nei[v,t][ii][1]:
                 w,t_p = (u,l_nei[v,t][ii][0])
-                print("from",(v,t), "next",w,t_p,)
                 contri_delta_svt(node, w, t_p, l_nei, sigma_r, contribution, deltasvvt, event, event_reverse, pre, GT, unt)
                 (t1,t2) = pre[w][t_p][v,t]
-                print("lol","v", v, "t", t, "w",w,"t_p",t_p)
                 if t1 != t2 and t_p > t and unt[v][t] >=t_p:
                     for yp,tpp in GT[t1,t2].successors((v,t)):
-                        print("*!*!*!*!*!*!  instant graph","v",v,"t",t,"t1",t1,"t2",t2, "w",w,"t_p",t_p, "yp", yp, "tpp", tpp)
                         contri_delta_svt(node, yp, tpp, l_nei, sigma_r, contribution, deltasvvt, event, event_reverse, pre, GT, unt)
                         ev_prev = prev_event(tpp,event,event_reverse)
                         r = GT[t1,t2].edge_weight((v,t),(yp,tpp),"weight")
@@ -131,17 +85,12 @@ def contri_delta_svt(node, v, t, l_nei, sigma_r, contribution, deltasvvt, event,
                 else:
                     partial_sum[l_nei[v,t][ii][0]] += s.copy()
 
-                print("******** half call contri_delta_svt","v", v, "t", t, "sum", s, "ii", ii, "partial_sum", partial_sum)
+
                 if ii != 0:
                     jj = event_reverse[l_nei[v,t][ii-1][0]]
                 else:
                     jj = event_reverse[t]
-                print("u", u, "dic_nodes[u]", l_nei[v,t][ii])
-                print("dic_nodes[u]",l_nei[v,t],"u",u)
-
                 contri_intermeidary_vertices(v, t, w, t_p, l_nei, partial_sum, contrib_local, ii, jj, sigma_r, event, event_reverse, contribution)
-
-
 
         if v not in contribution:
             contribution[v] = dict()
@@ -149,6 +98,5 @@ def contri_delta_svt(node, v, t, l_nei, sigma_r, contribution, deltasvvt, event,
             for ss in contrib_local[vv]:
                 contribution[vv][ss] = contrib_local[vv][ss]
         contribution[v][t] = s + deltasvvt[(v,t)]
-    print("******** end call contri_delta_svt","v", v, "t", t,"contribution[v][t]",contribution[v][t])
     return contribution
 
